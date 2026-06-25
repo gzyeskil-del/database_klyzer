3c8423a8377677627ac13e34e7ba9af9eced37b0
KLYZER WEBSITE <gzyeskil@gmail.com>
2026-06-25 23:32:08 +0800

    Initialize KLYZER WEBSITE database: akun.js

---
+++ database/akun.js
@@ -0,0 +1,29 @@
+[
+  {
+    "type": "profile",
+    "id": "profile_owner_kenzidev5",
+    "accountId": "owner_kenzidev5",
+    "telegramId": "1082147129",
+    "username": "Kenzidev5",
+    "passwordHash": "pbkdf2$c0b9e6e99f8e4c0ea8278a4b5d339dda$490ddc7349759497e1ebd5949faaa979dcca3a4504da320f0704295a572a7e2d",
+    "avatar": "",
+    "settings": {
+      "theme": "default",
+      "language": "id",
+      "botNotifications": true
+    },
+    "loginHistory": [],
+    "createdAt": "2026-06-25T15:32:04.086Z",
+    "updatedAt": "2026-06-25T15:32:04.088Z"
+  },
+  {
+    "id": "act_5b16540aa2eda0760fff",
+    "type": "activity",
+    "actorId": "system",
+    "action": "system.init",
+    "detail": "Inisialisasi empat berkas database dan akun Owner utama otomatis.",
+    "ip": "",
+    "userAgent": "",
+    "at": "2026-06-25T15:32:04.087Z"
+  }
+]
