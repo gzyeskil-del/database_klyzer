f48585df4395166212b27aeb5f218c3c161e9948
KLYZER WEBSITE <gzyeskil@gmail.com>
2026-06-26 03:13:13 +0800

    Process registration request: dataakun.js

--- database/dataakun.js
+++ database/dataakun.js
@@ -8,5 +8,15 @@
     "status": "active",
     "approvalStatus": "approved",
     "createdAt": "2026-06-25T15:32:04.086Z"
+  },
+  {
+    "type": "account",
+    "id": "usr_ea19bfb19a700740394f",
+    "telegramId": "1216074758",
+    "username": "Kenzidev51",
+    "role": "reseller",
+    "status": "active",
+    "approvalStatus": "approved",
+    "createdAt": "2026-06-25T19:13:09.437922Z"
   }
 ]
