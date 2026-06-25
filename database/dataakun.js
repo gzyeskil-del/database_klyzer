4cc7a41cb91e32f41be046195f3bf43c811c1c8e
KLYZER WEBSITE <gzyeskil@gmail.com>
2026-06-25 23:32:07 +0800

    Initialize KLYZER WEBSITE database: dataakun.js

--- database/dataakun.js
+++ database/dataakun.js
@@ -1,1 +1,15 @@
-[]
+[
+  {
+    "type": "account",
+    "id": "owner_kenzidev5",
+    "telegramId": "1082147129",
+    "username": "Kenzidev5",
+    "role": "owner",
+    "status": "active",
+    "approvalStatus": "approved",
+    "approvedBy": "system",
+    "approvedAt": "",
+    "createdAt": "2026-06-25T15:32:04.086Z",
+    "lastLoginAt": null
+  }
+]
