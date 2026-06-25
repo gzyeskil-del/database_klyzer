320cd63db01268b7b4a6ca4bc06975f4426ec65a
KLYZER WEBSITE <gzyeskil@gmail.com>
2026-06-26 04:11:53 +0800

    Initialize KLYZER WEBSITE Python database: users.js

--- database/users.js
+++ database/users.js
@@ -1,1 +1,16 @@
-[]
+export const users = [
+  {
+    "id": "owner_kenzidev5",
+    "username": "Kenzidev5",
+    "role": "owner",
+    "active": true,
+    "status": "active"
+  },
+  {
+    "id": "usr_ea19bfb19a700740394f",
+    "username": "Kenzidev51",
+    "role": "reseller",
+    "active": true,
+    "status": "active"
+  }
+];
